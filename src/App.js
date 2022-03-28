d0705aee9a0c1fd2c9f42b259fca28d0ca8bc1be
raniabenhadjali <benhadjaliraniaa015@gmail.com>
2022-03-28 12:36:05 +0200

    src

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import img2 from './img2.jpg';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{border:'solid 1px black'}}>
+
+<h1 className="title">Your name here</h1>
+
+<br/ >
+
+<img src={img2} />
+
+<br/ >
+
+<img src="/img1.jpg" />
+
+
+<video width="320" height="240" controls>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/VQc613pd-2E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+</video>
+</div>
+      
+   </div>
   );
 }
 
